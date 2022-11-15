452280c9467bd9af687fca327573f8d66ee52f13
Stefan Lehmann <bernd49634@web.de>
2022-11-15 01:59:38 +0100

    finalized UI for tx history

--- src/Components/Wallet.js
+++ src/Components/Wallet.js
@@ -72,51 +72,66 @@ const Wallet = (props) => {
           <Box>
             {props.isFetchedTX && props.normalTXHistory !== "" && (
               <Box>
-                <Box sx={{ border: "solid", borderColor: "white" }}></Box>
-                <Box
-                  variant={"h2"}
-                  component={"h3"}
-                  sx={{ marginLeft: "3vw", marginTop: "3vh" }}
-                >
-                  "Outgoing - transfer" Transaction History
-                </Box>
-                <Box
-                  style={{
-                    border: "solid",
-                    margin: "10px",
-                    padding: "10px",
-                    borderWidth: "1px",
-                  }}
-                >
-                  {props.normalTXHistory.data.result.length !== 0 &&
-                    props.normalTXHistory.data.result[0] !== undefined &&
-                    props.normalTXHistory.data.result.map((e, index) => {
-                      return (
-                        <Box
-                          key={index}
-                          style={{
-                            border: "solid",
-                            margin: "10px",
-                            padding: "10px",
-                            borderWidth: "1px",
-                            textAlign: "left",
-                          }}
-                        >
-                          <Typography variant={"h6"} component={"h3"}>
-                            This is the {index + 1 + "th"} TX in your List
-                          </Typography>
-                          <Box>
-                            Network =&gt;
-                            {props.normalTXHistory.data.result[index].network}
-                          </Box>
-                          <Box>
-                            Symbol =&gt;{" "}
-                            {
-                              props.normalTXHistory.data.result[index]
-                                .actions[0].metadata.symbol
-                            }{" "}
-                          </Box>
-                          <Box>
+                {props.normalTXHistory !== "" && (
+                  <Box>
+                    <Box sx={{ border: "solid", borderColor: "white" }}></Box>
+                    <Box
+                      variant={"h2"}
+                      component={"h3"}
+                      sx={{ marginLeft: "3vw", marginTop: "3vh" }}
+                    >
+                      "Outgoing - transfer" Transaction History
+                    </Box>
+                    <Box
+                      style={{
+                        border: "solid",
+                        margin: "10px",
+                        padding: "10px",
+                        borderWidth: "1px",
+                      }}
+                    >
+                      {
+                        /* props.normalTXHistory.data.result.length !== 0 &&
+                    props.normalTXHistory.data.result[0] !== undefined && */
+                        props.normalTXHistory.data.result.map((e, index) => {
+                          return (
+                            <Box
+                              key={index}
+                              style={{
+                                border: "solid",
+                                margin: "10px",
+                                padding: "10px",
+                                borderWidth: "1px",
+                                textAlign: "left",
+                              }}
+                            >
+                              <Typography variant={"h6"} component={"h3"}>
+                                This is the {index + 1 + "th"} TX in your List{" "}
+                                <img
+                                  class="image floatRight"
+                                  sx={{ textAlign: "right" }}
+                                  src={
+                                    props.normalTXHistory.data.result[index]
+                                      .actions[0].metadata.image
+                                  }
+                                  alt="token"
+                                ></img>
+                              </Typography>
+                              <Box>
+                                Network =&gt;
+                                {
+                                  props.normalTXHistory.data.result[index]
+                                    .network
+                                }
+                              </Box>
+                              <Box>
+                                Symbol =&gt;{" "}
+                                {
+                                  props.normalTXHistory.data.result[index]
+                                    .actions[0].metadata.symbol
+                                }{" "}
+                              </Box>
+                              {/*  <Box>
                             <img
                               class="image"
                               src={
@@ -125,77 +140,83 @@ const Wallet = (props) => {
                               }
                               alt="token"
                             ></img>
-                          </Box>
-                          <Box>
-                            Name =&gt;{" "}
-                            {
-                              props.normalTXHistory.data.result[index]
-                                .actions[0].metadata.name
-                            }{" "}
-                          </Box>
-                          <Box>
-                            Timestamp =&gt;
-                            {props.normalTXHistory.data.result[index].timestamp}
-                          </Box>
-                          <Box>
-                            From =&gt;{" "}
-                            {
-                              props.normalTXHistory.data.result[index]
-                                .address_from
-                            }
-                          </Box>
-                          <Box>
-                            To =&gt;{" "}
-                            {
-                              props.normalTXHistory.data.result[index]
-                                .address_to
-                            }
-                          </Box>
-                          <Box>
-                            Value token =&gt;{" "}
-                            {
-                              props.normalTXHistory.data.result[index]
-                                .actions[0].metadata.value_display
-                            }{" "}
-                          </Box>
-                          <Box>
-                            Value send =&gt;{" "}
-                            {
-                              props.normalTXHistory.data.result[index]
-                                .actions[0].metadata.value
-                            }{" "}
-                            Wei
-                          </Box>
+                          </Box> */}
+                              <Box>
+                                Name =&gt;{" "}
+                                {
+                                  props.normalTXHistory.data.result[index]
+                                    .actions[0].metadata.name
+                                }{" "}
+                              </Box>
+                              <Box>
+                                Timestamp =&gt;
+                                {
+                                  props.normalTXHistory.data.result[index]
+                                    .timestamp
+                                }
+                              </Box>
+                              <Box>
+                                From =&gt;{" "}
+                                {
+                                  props.normalTXHistory.data.result[index]
+                                    .address_from
+                                }
+                              </Box>
+                              <Box>
+                                To =&gt;{" "}
+                                {
+                                  props.normalTXHistory.data.result[index]
+                                    .address_to
+                                }
+                              </Box>
+                              <Box>
+                                Value token =&gt;{" "}
+                                {
+                                  props.normalTXHistory.data.result[index]
+                                    .actions[0].metadata.value_display
+                                }{" "}
+                              </Box>
+                              <Box>
+                                Value send =&gt;{" "}
+                                {
+                                  props.normalTXHistory.data.result[index]
+                                    .actions[0].metadata.value
+                                }{" "}
+                              </Box>
 
-                          <Box>
-                            Gas Used =&gt;{" "}
-                            {props.normalTXHistory.data.result[index].fee}
-                          </Box>
-                          {/* <Box>
+                              <Box>
+                                Gas Used =&gt;{" "}
+                                {props.normalTXHistory.data.result[index].fee}{" "}
+                                in Wei
+                              </Box>
+                              {/* <Box>
                             Gas Price =&gt;
                             {props.normalTXHistory.data.result[index].gasPrice}
                           </Box> */}
-                          <Box>
-                            Hash of TX =&gt;{" "}
-                            {props.normalTXHistory.data.result[index].hash}
-                          </Box>
+                              <Box>
+                                Hash of TX =&gt;{" "}
+                                {props.normalTXHistory.data.result[index].hash}
+                              </Box>
 
-                          <Box>
-                            Type =&gt;
-                            {props.normalTXHistory.data.result[index].type}
-                          </Box>
+                              <Box>
+                                Type =&gt;
+                                {props.normalTXHistory.data.result[index].type}
+                              </Box>
 
-                          <br></br>
-                        </Box>
-                      );
-                    })}
-                  <Button
-                    variant={"contained"}
-                    onClick={(e) => props.addNum(e)}
-                  >
-                    Load More
-                  </Button>
-                </Box>
+                              <br></br>
+                            </Box>
+                          );
+                        })
+                      }
+                      <Button
+                        variant={"contained"}
+                        onClick={(e) => props.addNum(e)}
+                      >
+                        Load More
+                      </Button>
+                    </Box>{" "}
+                  </Box>
+                )}
               </Box>
             )}
           </Box>
